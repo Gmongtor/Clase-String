7c17353e9387e9687a6e48d8cca569d1b82af349
Gmongtor <gmongtor@myuax.com>
2023-11-23 12:25:51 +0100

    Commit 1

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string nombre;
+    cout << "Ingrese del alumno: ";
+    cin >> nombre;
+    cout << "Hola " << nombre << endl;
+    string profesor;
+    cout << "Ingrese el nombre del profesor: ";
+    cin >> profesor;
+    cout << "Buenos dias, " << profesor << endl;
+    return 0;
+}
