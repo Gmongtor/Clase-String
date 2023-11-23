a394ad32b0c4f9d0562f086d8f2e5af58f69de07
Gmongtor <gmongtor@myuax.com>
2023-11-23 12:54:56 +0100

    commit 4 1

---
+++ Ejercicio 4/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string sentence = "El lenguaje C++ es poderoso.";
+    std::string sub_str = "C++";
+    std::string replacement = "Python";
+
+    std::cout << "Cadena original: " << sentence << std::endl;
